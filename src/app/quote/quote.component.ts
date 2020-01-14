3b1760cb535ab8943ada498083f85401eae645ca
muli3203 <muli3203@gmail.com>
2020-01-14 07:59:54 +0300

    changed the information on the codes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Quote(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+    new Quote(1, 'Either get busy living or get busy dying', '-S.K',new Date(2003,3,14)),
+    new Quote(2,'Remember, Hope is a good thing, maybe the best of things, and no good thing ever dies','-S.K',new Date(2019,6,9)),
+    new Quote(3,'You can not deny laughter;when it comes,it plops down in your favourite chair and stays as long as it wants','-S.K',new Date(2018,1,12)),
+    
   ];
 
   toggleDetails(index){
@@ -29,6 +27,13 @@ export class QuoteComponent implements OnInit {
         this.quotes.splice(index,1)
       }
     }
+  }
+
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
 
   constructor() { }

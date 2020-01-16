20289cf816fcd4a123f24b3b69347bfeb9f48f2a
muli3203 <muli3203@gmail.com>
2020-01-16 12:12:59 +0300

    Add two properties that control the create date function

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,8 @@
 export class Quote {
+    showCreatedDate: boolean;
     showDescription: boolean;
-    constructor(public id: number, public name: string, public description: string, public completeDate: Date) {
+    constructor(public id: number, public name: string, public description: string, public createdDate: Date, public clickCounter:number) {
         this.showDescription = false;
+        this.showCreatedDate = false;
     }
 }

09311b0260fe364be7654bd6dd6cc039aeb3ac0b
muli3203 <muli3203@gmail.com>
2020-01-16 14:36:25 +0300

    Add changes that correspond to the downote that is the dislike button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   quotes:Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14),0,0),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9),0,0),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12),0,0),
   ];
   addQuote(quote: Quote):void{
     this.quotes.push(quote)
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,7 @@
 export class Quote {
     showCreatedDate: boolean;
     showDescription: boolean;
-    constructor(public id: number, public name: string, public description: string, public createdDate: Date, public clickCounter:number) {
+    constructor(public id: number, public name: string, public description: string, public createdDate: Date, public clickCounter:number, public dislikeCounter:number) {
         this.showDescription = false;
         this.showCreatedDate = false;
     }
